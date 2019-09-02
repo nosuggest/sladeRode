cdd25383f79609efb38baaf07b5eb8d06f84e58e
shataowei <taowei.sha@ymm56.com>
2019-09-03 00:19:51 +0800

    Default Changelist

--- src/main/org/nlpcn/commons/utils/StringPart/StringTest06.java
+++ src/main/org/nlpcn/commons/utils/StringPart/StringTest06.java
@@ -1,5 +1,7 @@
 package main.org.nlpcn.commons.utils.StringPart;
 
+import java.util.Arrays;
+
 public class StringTest06 {
     public static void main(String[] args) {
         //charAt
@@ -25,16 +27,37 @@ public class StringTest06 {
         System.out.println("www.index.com.info".indexOf("i", 6));
 
         //lastIndexOf(String str, int lastIndex)
-        System.out.println("www.index.com.info".lastIndexOf("i",100));
+        System.out.println("www.index.com.info".lastIndexOf("i", 100));
 
         //String是length方法，数组是length属性
         System.out.println("www.nba.com".length());
 
         //String.replace(String targetStr,String replacementStr)
-        System.out.println("www.baidu.com".replace(".com",".org"));
+        System.out.println("www.baidu.com".replace(".com", ".org"));
 
         //String[] split(String s)
         System.out.println("www.baidu.com".split("."));
+
+        //boolean startWith(String s)
+
+        // String subString(int beginIndex,int endIndex)
+        System.out.println("www.google.com".substring(0, 3));//www
+        System.out.println("www.google.com".substring(3));//.google.com
+
+        //char[] toCharArray();
+        char[] c2 = "www.xiaomi.com".toCharArray();
+        System.out.println(Arrays.toString(c2));
+
+        //toUpperCase
+        System.out.println("www.xiaomi.com".toUpperCase());
+
+        //toLowerCase
+        //String trim()
+        // String valueof(object obj)
+        Object o = null;
+        System.out.println(o);
+        System.out.println(String.valueOf(o));
+        System.out.println(o.toString());//空指针异常
 
     }
 }
---
+++ src/main/org/nlpcn/commons/utils/StringPart/StringTest07.java
@@ -0,0 +1,11 @@
+package main.org.nlpcn.commons.utils.StringPart;
+
+/*
+ *   正则表达式
+ *       \d 数字
+ *       \D 非数字
+ *       \w 英文字母
+ *       \W 非英文字母
+ * */
+public class StringTest07 {
+}
