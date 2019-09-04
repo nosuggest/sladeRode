c1605101592c7d63cc7bf0b2cfd63bc7fd2b6e7f
shataowei <taowei.sha@ymm56.com>
2019-09-05 00:39:06 +0800

    Default Changelist

---
+++ src/main/org/nlpcn/commons/utils/StringPart/StringTest08.java
@@ -0,0 +1,9 @@
+package main.org.nlpcn.commons.utils.StringPart;
+
+public class StringTest08 {
+    public static void main(String[] args) {
+        String s1 = "abc";
+        String s2 = new String("abc");
+        System.out.println(s1==s2);
+    }
+}
